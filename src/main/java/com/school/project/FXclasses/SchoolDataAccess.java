7048376e496110638818a18f282387ac36093322
Annina Henninger <annina.henninger@aon.at>
2019-06-24 15:56:39 +0200

    adds formular for student to class inserting, but not yet working

---
+++ src/main/java/com/school/project/FXclasses/SchoolDataAccess.java
@@ -0,0 +1,58 @@
+package com.school.project.FXclasses;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SchoolDataAccess {
+
+    private Connection conn;
+    private static final String studentTable = "student";
+    private static final String classTable = "teacherclasskey";
+
+    public SchoolDataAccess()
+            throws SQLException, ClassNotFoundException {
+
+        // Class.forName("org.hsqldb.jdbc.JDBCDriver" );
+
+        //Check if JDBC driver is available
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        //Open a connection
+        System.out.println("Connecting to database...");
+        conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/schoolTest" +
+                        "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+                "root",
+                "");
+
+        // connection to write to a file
+        conn.setAutoCommit(true);
+        conn.setReadOnly(false);
+    }
+
+    public void closeDb() throws SQLException {
+        conn.close();
+    }
+    /**
+     * Get all db records
+     * @return
+     * @throws SQLException
+     */
+    public List<Student> getAllRowsStudents()  throws SQLException {
+
+        String sql = "SELECT studentId, stud_firstName, stud_lastName, stud_class FROM " + studentTable + " ORDER BY stud_lastName";
+        PreparedStatement pstmnt = conn.prepareStatement(sql);
+        ResultSet rs = pstmnt.executeQuery();
+        List<Student> list = new ArrayList<>();
+
+        while (rs.next()) {
+            int studentID = rs.getInt("studentId");
+            String studFirstName = rs.getString("stud_firstName");
+            String studLastName = rs.getString("stud_lastName");
+            String studOldClass = rs.getString("stud_class");
+            list.add(new Student(studentID, studFirstName, studLastName, studOldClass));
+        }
+        pstmnt.close(); // also closes related result set
+        return list;
+    }
+}
--- src/main/java/com/school/project/FXclasses/Student.java
+++ src/main/java/com/school/project/FXclasses/Student.java
@@ -5,22 +5,13 @@ public class Student {
     private int studentID;
     private String studFirstName;
     private String studLastName;
-    private int studAge;
-    private String studEmail;
-    private String sudOldClass;
-    private String studPassword;
-    private int fkParentID;
+    private String studOldClass;
 
-    public Student(int studentID, String studFirstName, String studLastName, int studAge, String studEmail,
-                   String sudOldClass, String studPassword, int fkParentID) {
+    public Student(int studentID, String studFirstName, String studLastName, String studOldClass) {
         this.studentID = studentID;
         this.studFirstName = studFirstName;
         this.studLastName = studLastName;
-        this.studAge = studAge;
-        this.studEmail = studEmail;
-        this.sudOldClass = sudOldClass;
-        this.studPassword = studPassword;
-        this.fkParentID = fkParentID;
+        this.studOldClass = studOldClass;
     }
 
     public int getStudentID() {
@@ -47,43 +38,12 @@ public class Student {
         this.studLastName = studLastName;
     }
 
-    public int getStudAge() {
-        return studAge;
+    public String getStudOldClass() {
+        return studOldClass;
     }
 
-    public void setStudAge(int studAge) {
-        this.studAge = studAge;
-    }
-
-    public String getStudEmail() {
-        return studEmail;
-    }
-
-    public void setStudEmail(String studEmail) {
-        this.studEmail = studEmail;
-    }
-
-    public String getSudOldClass() {
-        return sudOldClass;
-    }
-
-    public void setSudOldClass(String sudOldClass) {
-        this.sudOldClass = sudOldClass;
-    }
-
-    public String getStudPassword() {
-        return studPassword;
-    }
-
-    public void setStudPassword(String studPassword) {
-        this.studPassword = studPassword;
-    }
-
-    public int getFkParentID() {
-        return fkParentID;
-    }
-
-    public void setFkParentID(int fkParentID) {
-        this.fkParentID = fkParentID;
+    @Override
+    public String toString() {
+        return studFirstName +" " + studLastName;
     }
 }
