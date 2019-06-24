05be48cddc0eb508592f9961349d6b6f566ad85a
Annina Henninger <annina.henninger@aon.at>
2019-06-24 11:45:46 +0200

    adds start for FX

--- src/main/java/com/school/project/ProjectApplication.java
+++ src/main/java/com/school/project/ProjectApplication.java
@@ -1,10 +1,17 @@
 package com.school.project;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.security.auth.login.AppConfigurationEntry;
+
 @SpringBootApplication
-public class ProjectApplication {
+public class ProjectApplication extends Application {
+    public void start(Stage primaryStage) throws Exception{
+
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
