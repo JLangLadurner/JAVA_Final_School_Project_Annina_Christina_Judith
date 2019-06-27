fdacc0754b0eaa37b48893167a24233c9ac5c4ef
judith <judithmarialang@gmail.com>
2019-06-27 10:36:04 +0200

    adds schoolClass table to db

---
+++ src/main/java/com/school/project/model/SchoolClass.java
@@ -0,0 +1,40 @@
+package com.school.project.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "schoolClass")
+public class SchoolClass {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "classId")
+    private int classId;
+    @Column(name = "className")
+    private String className;
+
+    public int getClassId() {
+        return classId;
+    }
+
+    public void setClassId(int classId) {
+        this.classId = classId;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+}
